200efbba25130b84da80720329794e4c47806573
Peng <wei-peng@users.noreply.github.com>
2017-08-27 17:54:56 +0800

    Make use of the return value of begin() and measure()

--- NDIR_RasPi_Python/example.py
+++ NDIR_RasPi_Python/example.py
@@ -2,9 +2,15 @@ import NDIR
 import time
 
 sensor = NDIR.Sensor(0x4D)
-sensor.begin()
+
+if sensor.begin() == False:
+    print("Adaptor initialization FAILED!")
+    exit()
 
 while True:
-    sensor.measure()
-    print("CO2 Concentration: " + str(sensor.ppm) + "ppm")
+    if sensor.measure():
+        print("CO2 Concentration: " + str(sensor.ppm) + "ppm")
+    else:
+        print("Sensor communication ERROR.")
+
     time.sleep(1)
