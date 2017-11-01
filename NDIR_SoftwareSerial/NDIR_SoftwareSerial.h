38ba76519255152687ebed2ed584f4d203f413a9
Peng <wei-peng@users.noreply.github.com>
2017-11-01 19:13:32 +0800

    Add calibration function

--- NDIR_SoftwareSerial/NDIR_SoftwareSerial.h
+++ NDIR_SoftwareSerial/NDIR_SoftwareSerial.h
@@ -5,10 +5,10 @@ You can get one of those products on
 http://sandboxelectronics.com
 
 Version:
-V1.0
+V1.1
 
 Release Date:
-2016-03-30
+2017-11-01
 
 Author:
 Tiequan Shao          info@sandboxelectronics.com
@@ -24,18 +24,20 @@ Please keep the above information when you use this code in your project.
 
 class NDIR_SoftwareSerial {
     public:
-		NDIR_SoftwareSerial(uint8_t rx_pin, uint8_t tx_pin);
+        NDIR_SoftwareSerial(uint8_t rx_pin, uint8_t tx_pin);
 
         uint32_t ppm;
 
         uint8_t  begin();
         uint8_t  measure();
+        void     calibrateZero();
 
     private:
         SoftwareSerial serial;
 
         uint8_t  parse(uint8_t *pbuf);
 
-	    static uint8_t  cmd_measure[9];
+        static uint8_t  cmd_measure[9];
+        static uint8_t  cmd_calibrateZero[9];
 };
 #endif
