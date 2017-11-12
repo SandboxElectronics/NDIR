1ec888759884a10ce4a5c73f4b211fcf2884d761
Adam <adamrosas1972@gmail.com>
2017-11-12 09:05:36 -0800

    add power control

--- NDIR_RasPi_Python/example.py
+++ NDIR_RasPi_Python/example.py
@@ -6,11 +6,19 @@ sensor = NDIR.Sensor(0x4D)
 if sensor.begin() == False:
     print("Adaptor initialization FAILED!")
     exit()
-
-while True:
+sensor.power_on()
+print("Power On")
+i = 0
+while i < 50:
+    if i == 25:
+        sensor.power_off()
+        print("Power Off")
+        sensor.ppm = -1
     if sensor.measure():
         print("CO2 Concentration: " + str(sensor.ppm) + "ppm")
     else:
         print("Sensor communication ERROR.")
 
     time.sleep(1)
+    i += 1
+
